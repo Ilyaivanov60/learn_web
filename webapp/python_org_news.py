c85dc8d735a3b2f465edd66cc52fd11ee48cd654
ILya Ivanov <ivanovrocket@gmail.com>
2022-09-09 16:28:28 -0400

    Добавил поддержку базы данных

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -15,16 +18,19 @@ def get_python_news():
     if html:
         soup = BeautifulSoup(html, 'html.parser')
         all_news = soup.find('ul', class_='list-recent-posts').findAll('li')
-        result_news = []
         for news in all_news:
             title = news.find('a').text
             url = news.find('a')['href']
-            print(url)
             published = news.find('time').text
-            result_news.append({
-                'title': title,
-                'url': url,
-                'published': published
-            })
-        return result_news
-    return False
+            try:
+                published = datetime.strptime(published, "%b. %d, %Y")
+            except ValueError:
+                published = datetime.strptime(published, "%B %d, %Y")
+            save_news(title, url, published)
+
+def save_news(title, url, published):
+    news_exits = News.query.filter(News.url == url).count()
+    if not news_exits:
+        new_news = News(title=title, url=url, published=published)
+        db.session.add(new_news)
+        db.session.commit()
